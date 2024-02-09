52bf1de6e0444d1ce1fb7a0b69bd303c6a31a059
Vicheak <suonvicheak991@gmail.com>
2024-02-09 17:42:01 +0700

    update service

--- src/main/java/com/vicheak/bank/loan/controller/LoanController.java
+++ src/main/java/com/vicheak/bank/loan/controller/LoanController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,9 @@ import com.vicheak.bank.loan.entity.Loan;
 import com.vicheak.bank.loan.mapper.LoanMapper;
 import com.vicheak.bank.loan.service.LoanService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/api/loans")
 public class LoanController {
@@ -36,7 +40,12 @@ public class LoanController {
 	}
 	
 	@GetMapping("/{customerId}")
-	public ResponseEntity<?> getByCustomerId(@PathVariable Long customerId){
+	public ResponseEntity<?> getByCustomerId(
+			@RequestHeader("vicheakbank-correlation-id") String correlationId,
+			@PathVariable Long customerId){
+		//System.out.println("================= Loan service is called ================");
+		
+		log.debug("Correlation id found : {}", correlationId);
 		return ResponseEntity.ok(loanService.getByCustomerId(customerId)); 
 	}
 
